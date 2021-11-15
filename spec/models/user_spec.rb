076f455f08e806d8873e1e1c0b1e96c6ad508720
SVRourke <samuelvanrourke@gmail.com>
2021-11-15 17:45:26 -0500

    migrated db, stubbed out reqs and tests for users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  # forms
+  # name
+    # has name
+    # invalid without name
+    # invalid with duplicate name
+  # email
+    # has email
+    # invalid without email
+    # invalid with duplicate email
+    # requires valid email
+  # password
+    # has a password
+    # invalid without password
+    # constrain password
+end
