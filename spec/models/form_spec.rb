903294419c192ac2248fe84d56e2db0b66296e5f
SVRourke <samuelvanrourke@gmail.com>
2021-11-15 17:48:33 -0500

    created and stubbed out form model, migration, and tests, added relations with user

---
+++ spec/models/form_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Form, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  # has submissions
+  # has inputs
+  # belongs to a user
+  # user cannot create duplicate named forms
+end
