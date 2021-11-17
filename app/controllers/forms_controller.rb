c4b7adfdabb2e827b505de516585622c3b252d6c
SVRourke <samuelvanrourke@gmail.com>
2021-11-17 13:28:42 -0500

    index method done, start show, added placeholder responses for non implemented actions

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,28 +1,31 @@
 class FormsController < ApplicationController
     def index
-        # get user id
-        # get forms for that user
-        # render form index
+        @user = current_user
+        @forms = @user.forms
     end
-
+    
     def show
+        @form = Form.find(params[:id])
         # get form id
         # get form
         # get submissions
         # render form page
     end
-
+    
     def new
+        render json: {message: "New"}
         # render new form page
     end
-
+    
     def create
+        render json: {message: "CREATE"}
         # get body
         # generate form and inputs for body
         # redirect to form show for new form
     end
-
+    
     def destroy
+        render json: {message: "Destroy"}
         # get form
         # detroy form
     end
