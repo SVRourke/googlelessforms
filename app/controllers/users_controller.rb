4fbd4d63f876a3446c48b46a7e34fc75ba936dea
SVRourke <samuelvanrourke@gmail.com>
2021-11-17 09:42:36 -0500

    fleshed out controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new()
         # render the page
     end
 
     def create
+        user = User.create(username: params[:username], email: params[:email], password: params[:password])
+
+        if user.save()
+            render json: {message: "user created"}
+            # redirect to user>form>index
+        else
+            flash[:error] = user.errors.full_messages
+            redirect_back(fallback_location: new_user_path)
+        end
         # try to create user
         # if good assign session id and redirect to forms index
         # if bad redirect back with flash for error
