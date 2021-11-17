7d947882febe177c25b9da145b63df38692743f2
SVRourke <samuelvanrourke@gmail.com>
2021-11-17 08:19:01 -0500

    added notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
     def new
+        # render the page
     end
 
     def create
+        # try to create user
+        # if good assign session id and redirect to forms index
+        # if bad redirect back with flash for error
     end
-
-    def destroy
-    end
-
 end
