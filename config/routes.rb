db42e98442a6297870249d440173fb1cd1e05b84
SVRourke <samuelvanrourke@gmail.com>
2021-11-17 16:28:42 -0500

    pulled in routes, suppressing default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Users > forms > inputs | submissions
+  
+  # Auth
+    post "login", to: "auth#create"
+    delete "login", to: "auth#destroy"
+
+  # Users
+  # TODO: add destroy
+  resources :user, only: [:new, :create] do
+    resources :forms, only: [:index, :show, :new, :create, :destroy]
+  end
+
+  resource :form, only: [] do
+    resource :submissions, only: [:new, :create]
+  end
+
 end
