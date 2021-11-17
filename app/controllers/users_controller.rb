481a41cf9536f91f1c62e6c7dba80b1050c2162c
SVRourke <samuelvanrourke@gmail.com>
2021-11-17 10:00:08 -0500

    updated login on creation and relevant redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,18 @@
 class UsersController < ApplicationController
     def new
         @user = User.new()
-        # render the page
     end
 
     def create
+        # try to create user
         user = User.create(username: params[:username], email: params[:email], password: params[:password])
 
         if user.save()
-            render json: {message: "user created"}
-            # redirect to user>form>index
+            session[:user_id] = user.id
+            redirect_to :user_forms
         else
             flash[:error] = user.errors.full_messages
             redirect_back(fallback_location: new_user_path)
         end
-        # try to create user
-        # if good assign session id and redirect to forms index
-        # if bad redirect back with flash for error
     end
 end
