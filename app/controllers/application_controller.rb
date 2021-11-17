bfd3754fca81c251a0174b2aed054341078f86c2
SVRourke <samuelvanrourke@gmail.com>
2021-11-17 16:48:10 -0500

    removed old methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::API
 
-    def current_user
-        if logged_in?
-            @user ||= User.find(session[:user_id]) 
-        end
-    end
-
-    def logged_in?
-        !!session[:user_id]
-    end
 end
