0cc88e3995fccaad3300c3d7f4c113962b59d202
SVRourke <samuelvanrourke@gmail.com>
2021-11-17 13:28:04 -0500

    added methods for checking if a user is logged in and finding the logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        if logged_in?
+            @user ||= User.find(session[:user_id]) 
+        end
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
 end
