ace81650d399cef38dc35861006176b24c12b097
SVRourke <samuelvanrourke@gmail.com>
2021-11-18 17:34:23 -0500

    added a method for strong params for creating inputs, completed index, create and destroy actions

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -1,2 +1,30 @@
 class InputsController < ApplicationController
+    def index
+        inputs = Input.where(form_id: params[:form_id])
+        render json: inputs
+    end
+    
+    def create
+        form = Form.find_by(user_id: params[:user_id], id: params[:form_id])
+        input = form.inputs.new(input_params)
+
+        if input.save()
+            render json: input
+        else
+            render json: input.errors.full_messages
+        end
+    end
+    
+    def destroy
+        input = Input.find_by(form_id: params[:form_id], id: params[:id])
+        input.destroy()
+        render json: {message: "removed input"}
+    end
+
+    private
+
+    def input_params
+        params.require(:input).permit(:inputType, :label, :name, :required)
+    end
+
 end
