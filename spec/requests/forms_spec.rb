ec04767ade70c99ffcb173fabf41ba134b0faf13
SVRourke <samuelvanrourke@gmail.com>
2021-11-18 14:31:29 -0500

    layed out notes for needed tests

--- spec/requests/forms_spec.rb
+++ spec/requests/forms_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Forms", type: :request do
-  describe "GET /index" do
+  describe "#Index" do
     pending "add some examples (or delete) #{__FILE__}"
+    # returns forms for a specific user
+    # does not return forms for a different user
+  end
+  
+  describe "#Show" do
+    pending "add some examples (or delete) #{__FILE__}"
+    # returns the specified form
+    # only returns the form if it is the user's form
+    # returns an error message if form does not exist
+  end
+  
+  describe "#Create" do
+    pending "add some examples (or delete) #{__FILE__}"
+    # creates a new form
+    # returns an error message if there are errors
+  end
+
+  describe "#Destroy" do
+    pending "add some examples (or delete) #{__FILE__}"
+    # destroys the given form
+    # only destroy form owned by the user
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
+  describe "#CREATE" do
     pending "add some examples (or delete) #{__FILE__}"
+    # creates a new user
+    # returns an error message if there are any errors
+  end
+
+  describe "#DESTROY" do
+    pending "add some examples (or delete) #{__FILE__}"
+    # destroys the logged in user
+    # cannot destroy a user other than the logged in user
   end
 end
