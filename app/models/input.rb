717becccbb286b120112bbdcffe7d3dcfc9c1509
SVRourke <samuelvanrourke@gmail.com>
2021-11-19 08:54:43 -0500

    accepting nested attributes for valueOptions

--- app/models/input.rb
+++ app/models/input.rb
@@ -2,6 +2,7 @@ class Input < ApplicationRecord
     validates :inputType, inclusion: {in: %w(date checkbox email number text longtext telephone time url file), message: "%{value} is not a valid input type"}
     belongs_to :form
     has_many :valueOptions
+    accepts_nested_attributes_for :valueOptions
 end
 
 # submit (default)
