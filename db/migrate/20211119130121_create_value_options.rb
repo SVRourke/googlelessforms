42ff8fad208a783cea844c9ddc41abfa6b679b01
SVRourke <samuelvanrourke@gmail.com>
2021-11-19 08:11:23 -0500

    added reference to input and column for value

--- db/migrate/20211119130121_create_value_options.rb
+++ db/migrate/20211119130121_create_value_options.rb
@@ -1,7 +1,8 @@
 class CreateValueOptions < ActiveRecord::Migration[6.1]
   def change
     create_table :value_options do |t|
-
+      t.references :input
+      t.string :value
       t.timestamps
     end
   end
