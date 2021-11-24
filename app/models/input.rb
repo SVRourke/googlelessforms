fa10f240908b05edfaf12d19b50ee2b9467eda40
SVRourke <samuelvanrourke@gmail.com>
2021-11-24 15:35:25 -0500

    added cascading delete for valueOptions

--- app/models/input.rb
+++ app/models/input.rb
@@ -1,7 +1,7 @@
 class Input < ApplicationRecord
     validates :inputType, inclusion: {in: %w(date checkbox email number text longtext telephone time url file), message: "%{value} is not a valid input type"}
     belongs_to :form
-    has_many :valueOptions
+    has_many :valueOptions, dependent: :destroy
     accepts_nested_attributes_for :valueOptions
 end
 
