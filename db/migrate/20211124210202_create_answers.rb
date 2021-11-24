62dd1ead1590873566197888e0489aacf6f94d6d
SVRourke <samuelvanrourke@gmail.com>
2021-11-24 16:06:53 -0500

    generated answer model, migration and spec

---
+++ db/migrate/20211124210202_create_answers.rb
@@ -0,0 +1,9 @@
+class CreateAnswers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :answers do |t|
+      t.string :name
+      t.string :value
+      t.timestamps
+    end
+  end
+end
