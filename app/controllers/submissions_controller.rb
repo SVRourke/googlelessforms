73262f0e7786bfdde5c63f0ce19fa71f4c4e4f13
SVRourke <samuelvanrourke@gmail.com>
2021-11-26 09:39:17 -0500

    built index and create actions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,24 @@
 class SubmissionsController < ApplicationController
+    def index
+        submissions = Submission.where(form_id: params[:form_id])
+        render json: submissions, :include => :answers    
+    end
+
+    def create
+        form = Form.find(params[:form_id])
+        sub = form.submissions.new(strong_params)
+
+        if sub.save()
+            render json: sub, include: :answers 
+        else
+            render json: sub.errors.full_messages
+        end
+    end
+
+    private
+
+    def strong_params
+        params.require(:submission).permit(answers_attributes: [:name, :value])
+    end
+
 end
